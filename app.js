092d28ceed4eedfada764001543f7cd58f0a4e97
arashad202 <ahm.rashad.202@gmail.com>
2025-06-10 12:02:02 +0300

    Change input value
    Add event listener to number to change input value.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+let myScreen = document.querySelector('input');
+const myNumbers = document.querySelectorAll('.num');
+const myOperator = document.querySelectorAll('.operator');
+const reset = document.querySelector('.reset');
+const myResult = document.querySelector('.result');
+const myDelete = document.querySelector('delete');
+let result = '';
+
+myNumbers.forEach(num => {
+    num.addEventListener('click', () => {
+        result += num.textContent;
+        myScreen.value = result;
+    })
+})
+
 const add = (a,b) => a + b;
 const subtract = (a,b) => a-b;
 const multiply = (a,b) => a*b;
 const divide = (a,b) => a/b;
 
-let result = 0;
 
 const operate = (num1, num2, operator) => {
 
