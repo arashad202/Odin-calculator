54df7d6e6aefc199f8ff12f1b071f63ca8528a2d
arashad202 <ahm.rashad.202@gmail.com>
2025-06-10 11:06:57 +0300

    Initial commit with boilerplate

---
+++ app.js
@@ -0,0 +1,28 @@
+const add = (a,b) => a + b;
+const subtract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => a/b;
+
+let result = 0;
+
+const operate = (num1, num2, operation) => {
+
+    switch (operation) {
+        case '+':
+            result = add(num1,num2);
+            break;
+        case '-':
+            result = subtract(num1,num2);
+            break;
+        case '*':
+            result = multiply(num1,num2);
+            break;
+        case '/':
+            result = divide(num1,num2);
+            break;
+        default:
+            result = 0;
+    };
+
+    return result;
+}
